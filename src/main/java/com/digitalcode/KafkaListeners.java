e1068ae77a914528ac678ff9758d6c3e78ba21c3
achraf-fandouli <achraf.fandouli@gmail.com>
2023-10-24 23:55:24 +0100

    Set up a listener to consume the published messages.

---
+++ src/main/java/com/digitalcode/KafkaListeners.java
@@ -0,0 +1,13 @@
+package com.digitalcode;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaListeners {
+
+	@KafkaListener(topics = "digitalcode", groupId = "groupId")
+	void listener(String data) {
+		System.out.println("Listener received: " + data + "\"😀\"");
+	}
+}
